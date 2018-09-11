c273d1ca43f3d2956eec03d2315061ae380486bb
Xavier Bruhiere <xavier.bruhiere@gmail.com>
2018-09-11 14:29:28 +0800

    feat(): add uuid, fix git, fix stream

--- stream.py
+++ stream.py
@@ -9,6 +9,26 @@ def parse(line):
         yield kv.split('=')
 
 
+def cant_die(func):
+    def _inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            print('failed to parse line: {}'.format(e))
+    return _inner
+
+
+@cant_die
+def process_history(line):
+    print('\n===  New command  ===')
+    print(line)
+    pp({k: v for k, v in parse(line)}, indent=4)
+    # FIXME for some reason it doesn't work piping to `jq`
+    # import json
+    # sys.stdout.write(json.dumps({k: v for k, v in parse(line)}))
+    # sys.stdout.write('\r\n')
+
+
 if __name__ == '__main__':
     # TODO handle ctrl-c
     while 1:
@@ -16,9 +36,4 @@ if __name__ == '__main__':
         if not line:
             continue
 
-        print(line)
-        pp({k: v for k, v in parse(line)}, indent=4)
-        # FIXME for some reason it doesn't work piping to `jq`
-        # import json
-        # sys.stdout.write(json.dumps({k: v for k, v in parse(line)}))
-        # sys.stdout.write('\n')
+        process_history(line)
